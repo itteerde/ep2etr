08e9b34eb8869e7f6db990b84259ba6b1c270f01
Erik Itter <erik.itter@gmail.com>
2026-02-01 17:30:43 +0100

    issue #12: refactor and prep structure for lessen

--- macros/gm/Explosives.mjs
+++ macros/gm/Explosives.mjs
@@ -32,6 +32,7 @@ class LibItteerdeEp2e {
      * @param {*} initiating_target 
      * @param {*} affected_roll 
      * @param {*} affected_target 
+     * @returns 1 if initiating party wins, 0 otherwise. Numberical for counting.
      */
     static classifyOpposed(initiating_roll, initiating_target, affected_roll, affected_target) {
     }
@@ -41,26 +42,33 @@ class LibItteerdeEp2e {
      * 
      * @param {*} roll 
      * @param {*} target 
+     * @returns 1 if successful, otherwise 0. Numerical for counting.
      */
     static classifyUnOpposed(roll, target) {
+        if (roll <= target) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Compute distance between a and b.
+     * 
+     * @param {*} a "source" for this macro
+     * @param {*} b "affected" for this macro
+     * @param {*} zA elevation of "source". Note that MeasuredTemplate has an elevation.
+     * @returns the distance between a and b.
+     */
+    static distance(a, b, zA = 0) {
+        return (
+            (a.position.x - b.position.x) ** 2 +
+            (a.position.y - b.position.y) ** 2 +
+            ((zA != 0 ? zA : a.document.elevation) - b.document.elevation) ** 2
+        ) ** 0.5;
     }
 }
 
-/**
- * Compute distance between a and b.
- * 
- * @param {*} a "source" for this macro
- * @param {*} b "affected" for this macro
- * @param {*} zA elevation of "source". Note that MeasuredTemplate has an elevation.
- * @returns the distance between a and b.
- */
-function distance(a, b, zA = 0) {
-    return (
-        (a.position.x - b.position.x) ** 2 +
-        (a.position.y - b.position.y) ** 2 +
-        ((zA != 0 ? zA : a.document.elevation) - b.document.elevation) ** 2
-    ) ** 0.5;
-}
 
 // https://foundryvtt.com/api/classes/foundry.canvas.placeables.MeasuredTemplate.html
 class ExtendedTemplate extends foundry.canvas.placeables.MeasuredTemplate {
